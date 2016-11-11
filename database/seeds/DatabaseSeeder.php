7caf0212667b15e2392efeac64b5d2b518f5b6e0
Isaac Lebwohl-Steiner <Isaac Lebwohl-Steiner>
2016-11-11 03:13:38 -0200

    Created seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(SexesTableSeeder::class);
+        $this->call(CharactersTableSeeder::class);
     }
 }
