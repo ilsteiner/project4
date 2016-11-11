7112649643cb6cdf4d090849bc2e6267e1bcc2d6
Isaac Lebwohl-Steiner <Isaac Lebwohl-Steiner>
2016-11-10 22:46:52 -0200

    Added controllers and the resource route for characters

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace CharDB\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use CharDB\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace CharDB\Http\Controllers\Auth;
 
-use App\User;
-use App\Http\Controllers\Controller;
+use CharDB\User;
+use CharDB\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
