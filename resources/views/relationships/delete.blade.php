1ae4936f52667375d9a04537745f6e04119446fa
Isaac Steiner <ilebwohlsteiner@gmail.com>
2016-12-09 00:33:16 -0200

    Need to add nav on xs screens and add login

---
+++ resources/views/relationships/delete.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.master')
+
+@section('page')
+Deleting {{ $relationship->to_string }}
+@stop
+
+@section('content')
+	<div class="row">
+		<div class="col-md-12">
+			<div class="jumbotron">
+				<h1>You are about to delete a {{ $relationship->to_string }}! The ID is {{ $relationship->id }}.</h1>
+				<h2>Are you sure you want to do this?</h2>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6">
+			<form action="/relationships/{{ $relationship->id }}" method="POST">
+				{{ method_field('DELETE') }}
+				{{ csrf_field() }}
+				<input type="text" name="id" hidden readonly value="{{ $relationship->id }}"/>
+				<button type="submit" class="btn-danger btn btn-lg btn-block">
+					<i class="fa fa-trash" aria-hidden="true"></i>
+					Yes! Destroy their connection!
+				</button>
+			</form>
+		</div>
+		<div class="col-md-6">
+			<a href="/relationships/show/{{ $relationship->id }}">
+				<button type="button" class="btn-info btn btn-lg btn-block">
+					<i class="fa fa-undo" aria-hidden="true"></i>
+					No! I didn't mean to do that!
+				</button>
+			</a>
+		</div>
+	</div>
+@stop
