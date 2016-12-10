cadf45a02791fc496d772627947f99cdc62caaf6
Isaac Steiner <ilebwohlsteiner@gmail.com>
2016-12-10 08:03:32 -0200

    Favicon compatibility

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,9 +4,20 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>CharDB - @yield('page','CharDB')</title>
-	<link rel="shortcut icon" href="/favicon.ico?v2" />
 	{{-- Includes bootstrap and other master styling, mixed down from multiple .sass files --}}
 	<link rel="stylesheet" href="/css/CharDB.css">
+
+	{{-- Favicon stuff, built using realfavicongenerator.net --}}
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=3">
+	<link rel="icon" type="image/png" href="/favicon-32x32.png?v=3" sizes="32x32">
+	<link rel="icon" type="image/png" href="/favicon-16x16.png?v=3" sizes="16x16">
+	<link rel="manifest" href="/manifest.json?v=3">
+	<link rel="mask-icon" href="/safari-pinned-tab.svg?v=3" color="#5bbad5">
+	<link rel="shortcut icon" href="/favicon.ico?v=3">
+	<meta name="apple-mobile-web-app-title" content="CharDB">
+	<meta name="application-name" content="CharDB">
+	<meta name="theme-color" content="#000000">
+
 	@yield('head')
 </head>
 <body>
