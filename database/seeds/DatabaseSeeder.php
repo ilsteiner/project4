d1e3bbc69daf4064a91934ca5c4ee460b531035e
Isaac Steiner <ilebwohlsteiner@gmail.com>
2016-12-11 22:10:05 -0200

    Added required user seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(SexesTableSeeder::class);
         $this->call(CharactersTableSeeder::class);
         $this->call(RelationshipsTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
